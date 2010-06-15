50f5ec5bafa212aa36bc66ef0e25ca7f67395485
Adam Savitzky <as127@.partners.org>
2010-06-15 16:02:14 -0400

    essage=configured

--- app/config/core.php
+++ app/config/core.php
@@ -200,12 +200,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'dlfj38udoih98r8rgf9wfga0wp0ashddh9s');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '4057230459235692450237493452048937');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
